524936068d1485a7ecd0729de7c8384b4e192f34
jcas12 <jarturocs0412@gmail.com>
2025-05-31 12:38:04 -0400

    Final changes to the code

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,31 @@
-
-
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div id="contact" className="flex min-h-screen w-full flex-col 
+    items-center justify-center gap-16 p-8">
+      <h1 className="text-center text-6xl font-light text-blue-600">Contact Me</h1>
+
+      <form className="flex w-full max-w-md flex-col gap-8 rounded-lg
+      p-6 md:max-w-lg lg:max-w-xl">
+        <div className="flex flex-col gap-4">
+          <input type="text" placeholder="Your Name"
+          className="rounded-lg border-2 border-blue-500 px-4 py-3 text-lg
+          outline-none transition-all duration-200
+          hover:bg-blue-50 focus:ring-2 focus:ring-blue-500"/>
+
+          <input type="text" placeholder="Email"
+          className="rounded-lg border-2 border-blue-500 px-4 py-3 text-lg
+          outline-none transition-all duration-200
+          hover:bg-blue-50 focus:ring-2 focus:ring-blue-500"/>
+        </div>
+
+        <textarea placeholder="Your Message" className="h-32 w-full resize-none
+        rounded-lg border-2 border-blue-500 px-4 py-3 text-lg
+          outline-none transition-all duration-200
+          hover:bg-blue-50 focus:ring-2 focus:ring-blue-500"></textarea>
+        <button className="rounded-lg border-2 border-blue-500 bg-blue-500 
+        px-6 py-3 font-semibold text-white transition-all duration-200 hover:bg-blue-600">Send!</button>
+      </form>
+    </div>
   )
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,11 @@
 import { BiLogoGithub, BiLogoLinkedin } from 'react-icons/bi'
-import image from '../../public/julioSELFIE.jpg'
 
 const Home = () => {
   return (
     <div id="home" className="flex min-h-screen w-full items-center justify-center"> 
 
         <div className="flex flex-col items-center justify-center gap-8 p-5 text-center">
-            <img src={image} alt="" className='w-[250px]
-            sm:w-[300px] rounded-full'/>
+            <img src="/julioselfie.JPG" alt="" className='w-[250px] sm:w-[300px] rounded-full'/>
 
             <div className="space-y-1 sm:space-y-3">
                 <h1 className='bg-gradient-to-r from-blue-500 to-blue-600
@@ -16,8 +14,8 @@ const Home = () => {
                 <h2 className='bg-gradient-to-r from-blue-500 to-blue-600
                 bg-clip-text text-4xl font-semibold text-transparent md:text-2xl
                 lg:text-3xl'>Computer Science Senior</h2>
-                <p className='max-w-[500px] text-sm text-gray-500
-                '>Hey! My name is Julio Cuello. I am a rising Senior
+                <p className='max-w-[500px] text-sm text-gray-500'>
+                    Hey! My name is Julio Cuello. I am a rising Senior
                     that is currently studying Computer Science at the
                     Polytechnic University of Puerto Rico. I have a deep interest
                     in expanding my portfolio with more industry experience, 
@@ -25,17 +23,29 @@ const Home = () => {
                 </p>
             </div>
 
-            {/*Social Media Links! */}
+            {/* Social Media Links! */}
             <div className="flex gap-3">
-                <BiLogoGithub className='h-10 w-10 cursor-pointer rounded-full border-2
-                border-transparent bg-blue-600 p-2 text-white transition-all duration-200
-                hover:scale-110 hover:border-blue-600 hover:bg-white hover:text-blue-600
-                md:h-12 md:w-12'/>
+                <a 
+                  href="https://github.com/jcas12" 
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                >
+                  <BiLogoGithub className='h-10 w-10 cursor-pointer rounded-full border-2
+                  border-transparent bg-blue-600 p-2 text-white transition-all duration-200
+                  hover:scale-110 hover:border-blue-600 hover:bg-white hover:text-blue-600
+                  md:h-12 md:w-12'/>
+                </a>
 
-                <BiLogoLinkedin className='h-10 w-10 cursor-pointer rounded-full border-2
-                border-transparent bg-blue-600 p-2 text-white transition-all duration-200
-                hover:scale-110 hover:border-blue-600 hover:bg-white hover:text-blue-600
-                md:h-12 md:w-12'/>
+                <a 
+                  href="https://www.linkedin.com/in/julio-cuello-s/" 
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                >
+                  <BiLogoLinkedin className='h-10 w-10 cursor-pointer rounded-full border-2
+                  border-transparent bg-blue-600 p-2 text-white transition-all duration-200
+                  hover:scale-110 hover:border-blue-600 hover:bg-white hover:text-blue-600
+                  md:h-12 md:w-12'/>
+                </a>
             </div>
         </div>
     </div>
