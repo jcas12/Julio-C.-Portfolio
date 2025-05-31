be64e877ffcd8c47065fbdde3cf47b6a570fbb05
jcas12 <jarturocs0412@gmail.com>
2025-05-31 13:49:29 -0400

    Provided a contact form with formspree's functionality

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,26 +4,42 @@ const Contact = () => {
     items-center justify-center gap-16 p-8">
       <h1 className="text-center text-6xl font-light text-blue-600">Contact Me</h1>
 
-      <form className="flex w-full max-w-md flex-col gap-8 rounded-lg
-      p-6 md:max-w-lg lg:max-w-xl">
+      <form 
+        action="https://formspree.io/f/mpwrabzl" 
+        method="POST"
+        className="flex w-full max-w-md flex-col gap-8 rounded-lg p-6 md:max-w-lg lg:max-w-xl"
+      >
         <div className="flex flex-col gap-4">
-          <input type="text" placeholder="Your Name"
-          className="rounded-lg border-2 border-blue-500 px-4 py-3 text-lg
-          outline-none transition-all duration-200
-          hover:bg-blue-50 focus:ring-2 focus:ring-blue-500"/>
+          <input
+            type="text"
+            name="name" 
+            placeholder="Your Name"
+            required
+            className="rounded-lg border-2 border-blue-500 px-4 py-3 text-lg outline-none transition-all duration-200 hover:bg-blue-50 focus:ring-2 focus:ring-blue-500"
+          />
 
-          <input type="text" placeholder="Email"
-          className="rounded-lg border-2 border-blue-500 px-4 py-3 text-lg
-          outline-none transition-all duration-200
-          hover:bg-blue-50 focus:ring-2 focus:ring-blue-500"/>
+          <input
+            type="email"
+            name="email" 
+            placeholder="Email"
+            required
+            className="rounded-lg border-2 border-blue-500 px-4 py-3 text-lg outline-none transition-all duration-200 hover:bg-blue-50 focus:ring-2 focus:ring-blue-500"
+          />
         </div>
 
-        <textarea placeholder="Your Message" className="h-32 w-full resize-none
-        rounded-lg border-2 border-blue-500 px-4 py-3 text-lg
-          outline-none transition-all duration-200
-          hover:bg-blue-50 focus:ring-2 focus:ring-blue-500"></textarea>
-        <button className="rounded-lg border-2 border-blue-500 bg-blue-500 
-        px-6 py-3 font-semibold text-white transition-all duration-200 hover:bg-blue-600">Send!</button>
+        <textarea
+          name="message" 
+          placeholder="Your Message"
+          required
+          className="h-32 w-full resize-none rounded-lg border-2 border-blue-500 px-4 py-3 text-lg outline-none transition-all duration-200 hover:bg-blue-50 focus:ring-2 focus:ring-blue-500"
+        ></textarea>
+
+        <button
+          type="submit"
+          className="rounded-lg border-2 border-blue-500 bg-blue-500 px-6 py-3 font-semibold text-white transition-all duration-200 hover:bg-blue-600"
+        >
+          Send!
+        </button>
       </form>
     </div>
   )
